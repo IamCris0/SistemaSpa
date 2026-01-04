4cd0e0c143507a1e85c91f2235bc70cb12fb25c4
IamCris0 <117394860+IamCris0@users.noreply.github.com>
2026-01-04 15:40:19 -0500

    commit

---
+++ SistemaSpaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaSpaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
